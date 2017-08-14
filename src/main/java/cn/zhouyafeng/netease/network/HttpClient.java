a36b908bb5de3afffd5b8b4661a7c08fae9ef765
yaphone <zhouyaphone@163.com>
2017-08-14 22:57:04 +0800

    日常开发

--- src/main/java/cn/zhouyafeng/netease/network/HttpClient.java
+++ src/main/java/cn/zhouyafeng/netease/network/HttpClient.java
@@ -1,6 +1,11 @@
 package cn.zhouyafeng.netease.network;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 /**
  * @author https://github.com/yaphone
@@ -14,20 +19,28 @@ public class HttpClient {
         if(instance == null){
             synchronized (HttpClient.class){
                 if (instance == null){
-                    instance = new OkHttpClient();
+                    instance = new OkHttpClient().newBuilder().addInterceptor(
+                            new Interceptor() {
+                                @Override
+                                public Response intercept(Chain chain) throws IOException {
+                                    Request request = chain.request()
+                                            .newBuilder()
+                                            .addHeader("Accept", "*/*")
+                                            .addHeader("Accept-Encoding", "gzip,deflate,sdch")
+                                            .addHeader("Accept-Language", "zh-CN,zh;q=0.8,gl;q=0.6,zh-TW;q=0.4")
+                                            .addHeader("Connection", "keep-alive")
+                                            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+                                            .addHeader("Host", "music.163.com")
+                                            .addHeader("Referer", "http://music.163.com/search/")
+                                            .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36")
+                                            .build();
+                                    return chain.proceed(request);
+                                }
+                            }).build();
                 }
             }
         }
         return instance;
-    }
-
-    /**
-     * @Desc:
-     * @Date:2017/8/14 22:20
-     *
-     */
-    private void addHeader(){
-
     }
 
 
